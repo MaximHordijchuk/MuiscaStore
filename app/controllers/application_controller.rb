58d686e427b6f975ccf2285ca97121030afdc80e
max <hordijchuk.m.i@gmail.com>
2016-09-26 13:47:14 +0300

    Add cart to header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :categories
+  helper_method :categories, :cart_products_count
 
   def categories
     @categories = Category.all
+  end
+
+  def cart_products_count
+    cart = session[:cart]
+    amount = 0
+    cart.each_value { |val| amount += val } if cart
+    amount
   end
 end
