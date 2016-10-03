3ff95d9692f314f482e875fd30bd2a6c97fb6698
max <hordijchuk.m.i@gmail.com>
2016-10-03 18:48:58 +0300

    Change currency to Euro.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def active_class(link_path)
     current_page?(link_path) ? 'active' : ''
   end
+
+  def number_to_euro(number)
+    number_to_currency(number, unit: '€')
+  end
 end
