d73906680d014ba4c485ed1b6a9307123aec6d16
max <hordijchuk.m.i@gmail.com>
2016-10-04 10:43:28 +0300

    Add card validation.

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,8 @@ class Order < ActiveRecord::Base
   validates :name, :surname, presence: true, length: { in: 1..50 }
   validates :address, presence: true, length: { in: 1..200 }
   validates :phone, presence: true
+  validates_format_of :card_no, with: /\A\d{16}|(\d{4}-\d{4}-\d{4}-\d{4})\Z/i, on: :create
+  validates :card_type, presence: true, length: { in: 1..30 }
+  validates :expire_year, numericality: { greater_than_or_equal_to: 16, less_than_or_equal_to: 99  }
+  validates :expire_month, numericality: { greater_than: 1, less_than_or_equal_to: 12 }
 end
