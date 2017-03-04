5254db74c5ab3253dc8d99ff2624440ff831304c
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2017-03-04 13:32:35 +0200

    Fix gemfile. Fix user management buttons styles. Change database config

--- db/migrate/20161001161537_add_admin_to_users.rb
+++ db/migrate/20161001161537_add_admin_to_users.rb
@@ -1,5 +1,7 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def change
     add_column :users, :admin, :boolean, default: false
+
+    User.create!(email: 'admin@example.com', password: 'password', admin: true)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003162157) do
+ActiveRecord::Schema.define(version: 20161003083052) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
